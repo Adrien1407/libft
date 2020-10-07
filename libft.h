b83f3fb8847956a55072f1461b365b3b50720221
adrien lancelle <adrienlancelle@gmail.com>
2020-10-07 16:40:01 +0200

    lib creation

---
+++ libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrienlanlan <adlancel@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/07 16:00:02 by adrienlan         #+#    #+#             */
+/*   Updated: 2020/10/07 16:38:19 by adrienlan        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int ft_isalnum(int c);
+int ft_isdigit(int c);
+int ft_isalpha(int c);
+int ft_islower(int c);
+int ft_isupper(int c);
+typedef struct s_list
+{
+	void *content;
+	size_t content_size;
+	struct s_list *next;
+} t_list;
+
+#endif
